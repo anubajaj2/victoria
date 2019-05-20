5acaa33b9636a540cdf79bfde21ad54e1ac7b792
sweta <shweta.sap.2019@gmail.com>
2019-05-20 11:05:06 -0400

    Kacchi view nevigation by Sweta

--- project/client/controller/Kacchi.controller.js
+++ project/client/controller/Kacchi.controller.js
@@ -1,27 +1,11 @@
+/*global location*/
 sap.ui.define(
-  ["victoria/controller/BaseController",
-   "sap/ui/model/json/JSONModel"],
-  function (
-    BaseController,
-    JSONModel
-    ) {
-    "use strict";
-    return BaseController.extend("victoria.controller.Kacchi",{
+    ["victoria/controller/BaseController"],
+    function (BaseController) {
+        "use strict";
 
-      onInit:function(){
-        var oRouter = this.getRouter();
-        console.log;
-        oRouter.getRoute("Kacchi").attachMatched(this._onKacchiRouteMatched , this);
-      },
+        return BaseController.extend("victoria.controller.Kacchi", {
 
-      getRouter:function(){
-        return sap.ui.core.UIComponent.getRouterFor(this);
-      },
-
-      _onKacchiRouteMatched:function(){
-
-      }
+        });
 
     });
-  }
-);

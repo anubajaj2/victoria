bf6dfb57c5ea079bbdd5d9a3de170fff88b1f0c4
anubhav <anubhav.abap@gmail.com>
2019-05-23 21:22:31 +0530

    Demo changes finalized

--- project/client/controller/demo.controller.js
+++ project/client/controller/demo.controller.js
@@ -9,8 +9,36 @@ sap.ui.define(
           onValueHelp: function(){
             this.getCustomerPopup();
           },
+          onConfirm: function(oEvent){
+            //whatever customer id selected push that in local model
+            debugger;
+              var myData = this.getView().getModel("local").getProperty("/demoData");
+              myData.Customer = oEvent.getParameter("selectedItem").getBindingContextPath().split("'")[1];
+              this.getView().getModel("local").setProperty("/demoData", myData);
+          },
+          onMaterialSelect: function(oEvent){
+            //whatever material id selected push that in local model
+            debugger;
+              var myData = this.getView().getModel("local").getProperty("/demoData");
+              myData.Material = oEvent.getParameter("selectedItem").getKey();
+               this.getView().getModel("local").setProperty("/demoData", myData);
+          },
           onSave: function(){
-            
+            var that = this;
+            that.getView().setBusy(true);
+            var myData = this.getView().getModel("local").getProperty("/demoData");
+            myData.CrDate =  this.getView().byId("someDate").getDateValue();
+            this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/Demos",
+                                      "POST", {}, myData, this)
+            .then(function(oData) {
+    					that.getView().setBusy(false);
+    					sap.m.MessageToast.show("Data Saved Successfully");
+
+    				}).catch(function(oError) {
+    					that.getView().setBusy(false);
+    					var oPopover = that.getErrorMessage(oError);
+    				});
+            ;
           }
 
         });

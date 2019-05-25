e55295d5f17ae93c91323a271ae2e8db8ef793d7
shrinivas <srinivas0254@gmail.com>
2019-05-25 10:57:47 +0530

    posting pendingorders.controller.js just for navigating to detail section

--- project/client/controller/pendingOrders.controller.js
+++ project/client/controller/pendingOrders.controller.js
@@ -1,10 +1,8 @@
-sap.ui.define(["victoria/controller/BaseController"],
-function(BaseController){
-  return{BaseController.extend("victoria.controller.pendingOrders",{
-      onInit:function(){
 
-      }
-  }
-)};
-}
-);
+sap.ui.define(["victoria/controller/BaseController"],
+function (BaseController) {
+  "use strict";
+  return BaseController.extend("victoria.controller.pendingOrders", {
+    onInit: function () {}
+  });
+});

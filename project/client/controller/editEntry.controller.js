d0dd642eef831ea85ae731d4c8d4c8c8c66fece5
shrinivas <srinivas0254@gmail.com>
2019-05-27 13:03:52 +0530

    adding for editEntry view,controller and manifest.json target for this view

---
+++ project/client/controller/editEntry.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define(["victoria/controller/BaseController"],
+function (BaseController) {
+  "use strict";
+  return BaseController.extend("victoria.controller.editEntry",{
+    onInit: function () {
+
+    }
+
+  });
+
+});

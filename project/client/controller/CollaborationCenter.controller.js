b4247e4d5fdb2a9e0a6402d6be21d5705ee109a9
shrinivas <srinivas0254@gmail.com>
2019-05-27 19:06:20 +0530

    ccenter controller.js changes

--- project/client/controller/CollaborationCenter.controller.js
+++ project/client/controller/CollaborationCenter.controller.js
@@ -4,7 +4,9 @@ function (BaseController) {
   "use strict";
   return BaseController.extend("victoria.controller.CollaborationCenter", {
      onInit: function () {
-
-     }  
+     onValueHelpRequest: function () {
+       this.getMaterialPopup();
+     }
+     }
   });
 });

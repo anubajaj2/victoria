1e60bb82100d8781a38a6530a7e22e9aa215872d
sweta <shweta.sap.2019@gmail.com>
2019-05-28 13:12:44 -0400

    kacchi - editable table

--- project/client/controller/Kacchi.controller.js
+++ project/client/controller/Kacchi.controller.js
@@ -5,6 +5,37 @@ sap.ui.define(
         "use strict";
 
         return BaseController.extend("victoria.controller.Kacchi", {
+          onInit:function(){
+            var kachhi = [
+              {"PaggaNo" : "", "Weight" : "", "Tunch" : "", "Fine" : ""},
+              {"PaggaNo" : "", "Weight" : "", "Tunch" : "", "Fine" : ""},
+              {"PaggaNo" : "", "Weight" : "", "Tunch" : "", "Fine" : ""},
+              {"PaggaNo" : "", "Weight" : "", "Tunch" : "", "Fine" : ""},
+              {"PaggaNo" : "", "Weight" : "", "Tunch" : "", "Fine" : ""}
+            ];
+            var oModel = new sap.ui.model.json.JSONModel();
+            oModel.setData({"superman": kachhi});
+            this.getView().setModel(oModel,"kacciLocalModel");
+          },
+          onCustValueHelp: function(){
+            this.getCustomerPopup();
+          },
+
+          onConfirm: function(oEvent){
+            debugger;
+            //whatever customer id selected push that in local model
+              // var myData = this.getView().getModel("local").getProperty("/demoData");
+              // myData.Customer = oEvent.getParameter("selectedItem").getBindingContextPath().split("'")[1];
+              // this.getView().getModel("local").setProperty("/demoData", myData);
+              // added by sweta to populate the selected cust and text to the input field
+              var selCust = oEvent.getParameter("selectedItem").getLabel();
+              var selCustName = oEvent.getParameter("selectedItem").getValue();
+              if(selCust){
+                this.getView().byId("idCustNo").setValue(selCust);
+                this.getView().byId("idCustName").setText(selCustName);
+              }
+              // End of addition by Sweta
+          }
 
         });
 

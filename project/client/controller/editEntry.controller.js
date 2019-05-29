bf2c96cc71e19f5d24a994d1679bef858163fd1e
shrinivas <srinivas0254@gmail.com>
2019-05-29 13:04:25 +0530

    editentry controller.js and editentry fragment changes

--- project/client/controller/editEntry.controller.js
+++ project/client/controller/editEntry.controller.js
@@ -26,19 +26,19 @@ function (BaseController) {
 
     onValueHelpRequest: function () {
       this.CustomerPopup();
+    },
+    onConfirm: function (oEvent) {
+     var myData = this.getView().getModel("local").getProperty("/editEntryData");
+     var selCust = oEvent.getParameter("selectedItem").getLabel();
+     var selCustName = oEvent.getParameter("selectedItem").getValue();
+     this.getView().byId("idCust").setValue(selCust);
+     this.getView().byId("idCustText").setValue(selCustName);
+     myData.Customer=oEvent.getParameter("selectedItem").getBindingContextPath().split("'")[1];
+     this.getView().getModel("local").getProperty("/editEntryData",myData);
+
+
     }
-    // onConfirm: function(oEvent){
-    //   //whatever customer id selected push that in local model
-    //     var myData = this.getView().getModel("local").getProperty("/editEntryData");
-    //     myData.Customer = oEvent.getParameter("selectedItem").getBindingContextPath().split("'")[1];
-    //     this.getView().getModel("local").setProperty("/editEntryData", myData);
-    //     // added by sweta to populate the selected cust to the input field
-    //     var selCust = oEvent.getParameter("selectedItem").getLabel();
-    //     var selCustName = oEvent.getParameter("selectedItem").getValue();
-    //     this.getView().byId("idCust").setValue(selCust);
-    //     // this.getView().byId("idCustName").setText(selCustName);
-    //     // End of addition by Sweta
-  // }
+
   });
 
 });

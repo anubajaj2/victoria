6fac71e6ec99cdb4eb43937f3e4848e3692855a4
shrinivas <srinivas0254@gmail.com>
2019-05-29 16:25:37 +0530

    ccenter controller and fragment changes

--- project/client/controller/CollaborationCenter.controller.js
+++ project/client/controller/CollaborationCenter.controller.js
@@ -13,9 +13,15 @@ function (BaseController) {
        var selMatText = oEvent.getParameter("selectedItem").getValue();
        this.getView().byId("idMat").setValue(selMat);
        this.getView().byId("idMatText").setValue(selMatText);
-      var myData= this.getView().getModel("local").getProperty("/collaborationCenterData");
+      var myData= this.getView().getModel("local").getProperty("/collaborationData");
       myData.Material=oEvent.getParameter("selectedItem").getKey();
-      this.getView().getModel("local").getProperty("/collaborationCenterData",myData);
-     }
+      this.getView().getModel("local").getProperty("/collaborationData",myData);
+    },
+    onSave: function () {
+      debugger;
+      // this.getView().setBusy(true);
+      // var myData = this.getView().getModel("local").getProperty("/collaborationData");
+      // myData.Qty= this.getView().byId("idQty").get
+    }
   });
 });

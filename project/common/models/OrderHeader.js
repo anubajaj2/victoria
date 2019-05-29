cc9aecc7e0368d573191d247379c42db299a52c1
anubhav <anubhav.abap@gmail.com>
2019-05-29 23:00:31 +0530

    More stable

--- project/common/models/OrderHeader.js
+++ project/common/models/OrderHeader.js
@@ -12,13 +12,25 @@ module.exports = function(OrderHeader) {
       //read only order no from DB, not other columns pass date as yyyy-mm-dd
       ///ctx.instance.Date
       debugger;
-      var date = ctx.instance.Date;
-      date.setHours(0,0,0,0);
+      // var date = ctx.instance.Date;
+      // date.setHours(0,0,0,0);
+      // var dateTo = JSON.parse(JSON.stringify(date));
+      // dateTo.addDays(1);
+
+      var start = new Date(JSON.parse(JSON.stringify(ctx.instance.Date)));
+      start.setHours(0,0,0,0);
+
+      var end = new Date(JSON.parse(JSON.stringify(ctx.instance.Date)));
+      end.setHours(23,59,59,999);
       OrderHeader.find({
         where: {
           and: [{
             Date: {
-              gte: date
+              gt: start
+            }
+          }, {
+            Date: {
+              lt: end
             }
           }]
         },
@@ -30,7 +42,7 @@ module.exports = function(OrderHeader) {
         //sort the orders in descending order created today
         if(orders.length > 0){
           //if there are/is order created today sort and get next order no
-      
+
           orders.sort(function (a, b) {
 					  return b.OrderNo - a.OrderNo;
 					});

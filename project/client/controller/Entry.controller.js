7c48498bcc69eec5cd10c0c0493793f1c8aa6c10
shrinivas <srinivas0254@gmail.com>
2019-05-30 16:04:25 +0530

    changes made in entry.controller.js and entry2.fragment

--- project/client/controller/Entry.controller.js
+++ project/client/controller/Entry.controller.js
@@ -25,19 +25,41 @@ function (BaseController) {
     },
 
     onValueHelpRequest: function () {
+
       this.CustomerPopup();
+    },
+    onRadioButtonSelect: function (oEvent) {
+      
+
+      // var focus = sap.ui.getCore().byId("idweight");
+      // window.setTimeout(function(){
+     	// 		focus.focus();
+     	// 	},0);
+
+      //
+      // jQuery.sap.delayedCall(0, this, function() {
+      // sap.ui.getCore().byId("idweight").focus();
+    //   jQuery.sap.delayedCall(0, this, function() {
+    // this.byId("idweight").focus();
+ // });
+},
+
+    // var oWeight = this.getView().byId("idweight");
+    // jQuery.sap.delayedCall(0, this, function() {
+    //   oWeight.focus();
+    // });
+
+    onConfirm: function (oEvent) {
+     var myData = this.getView().getModel("local").getProperty("/EntryData");
+     var selCust = oEvent.getParameter("selectedItem").getLabel();
+     var selCustName = oEvent.getParameter("selectedItem").getValue();
+     this.getView().byId("idCust").setValue(selCust);
+     this.getView().byId("idCustText").setValue(selCustName);
+     myData.Customer=oEvent.getParameter("selectedItem").getBindingContextPath().split("'")[1];
+     this.getView().getModel("local").getProperty("/EntryData",myData);
+
+
     }
-    // onConfirm: function (oEvent) {
-    //  var myData = this.getView().getModel("local").getProperty("/EntryData");
-    //  var selCust = oEvent.getParameter("selectedItem").getLabel();
-    //  var selCustName = oEvent.getParameter("selectedItem").getValue();
-    //  this.getView().byId("idCust").setValue(selCust);
-    //  this.getView().byId("idCustText").setValue(selCustName);
-    //  myData.Customer=oEvent.getParameter("selectedItem").getBindingContextPath().split("'")[1];
-    //  this.getView().getModel("local").getProperty("/editEntryData",myData);
-    //
-    //
-    // }
 
   });
 

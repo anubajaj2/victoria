244b4b565db480f1f541201f1c0d7384594d40f2
shrinivas <srinivas0254@gmail.com>
2019-05-31 18:54:36 +0530

    made some changes in entry.controller.js

--- project/client/controller/Entry.controller.js
+++ project/client/controller/Entry.controller.js
@@ -51,7 +51,7 @@ function (BaseController) {
      myData.Remarks = this.getView().byId("idRemarks").getValue();
      myData.Weight = this.getView().byId("idweight").getValue();
      myData.Tunch = this.getView().byId("idtunch").getValue();
-     // myData.Due Date = this.getView().byId("DueDateId").getDateValue();
+     myData.DueDate = this.getView().byId("DueDateId").getDateValue();
      this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/EntryData",
                                "POST", {}, myData, this)
      .then(function(oData) {
@@ -62,7 +62,53 @@ function (BaseController) {
        that.getView().setBusy(false);
        var oPopover = that.getErrorMessage(oError);
      });
+   },
+   onDelete: function(){
+     debugger;
+     var aSelectedLines = this.byId("idTable").getSelectedItems();
+     if (aSelectedLines.length) {
+       for(var i=0; i < aSelectedLines.length; i++){
+         var myUrl = aSelectedLines[i].getBindingContext().sPath;
+         this.ODataHelper.callOData(this.getOwnerComponent().getModel(), myUrl,
+                                   "DELETE", {}, {}, this);
+       }
+     }
+   },
+   onClear: function(){
+     this.byId("DateId").setValue("");
+     this.byId("idCust").setValue("");
+     this.byId("idCustText").setValue("");
+     this.byId("idweight").setValue("0");
+     this.byId("idRemarks").setValue("");
+     this.byId("idCash").setValue("0");
+     this.byId("idGold").setValue("0");
+     this.byId("idSilver").setValue("0");
+
    }
+   // onUpdateFinished: function(oEvent){
+   //   debugger;
+   //   var oTable = oEvent.getSource();
+   //   var itemList = oTable.getItems();
+   //   var noOfItems = itemList.length;
+   //   var value1;
+   //   var id;
+   //   var cell;
+   //   debugger;
+   //   for (var i = 0; i < noOfItems; i++) {
+   //     //Read the GUID from the Screen
+   //     var customerId = oTable.getItems()[i].getCells()[2].getText();
+   //     var customerData = this.allMasterData.customers[customerId];
+   //     oTable.getItems()[i].getCells()[2].setText(customerData.CustomerCode + ' - ' + customerData.Name );
+   //
+   //     // var materialId = oTable.getItems()[i].getCells()[3].getText();
+   //     // var materialData = this.allMasterData.materials[materialId];
+   //     // oTable.getItems()[i].getCells()[3].setText(materialData.ProductCode + ' - ' + materialData.ProductName );
+   //
+   //     //Find the customer data for that Guid in customer collection
+   //     //Change the data on UI table with semantic information
+   //   }
+   // }
+
 
   });
 

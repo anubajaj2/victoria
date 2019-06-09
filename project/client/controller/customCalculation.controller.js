d77787068ca5468a647c80d08ea0acfaef81b183
karpagam <karpagamit5690@gmail.com>
2019-06-09 12:07:25 +0530

    commit formatter

--- project/client/controller/customCalculation.controller.js
+++ project/client/controller/customCalculation.controller.js
@@ -3,12 +3,13 @@ sap.ui.define([
   "sap/ui/model/json/JSONModel",
   "sap/ui/core/routing/History",
   "sap/m/MessageToast",
+  "victoria/models/formatter"
 ],
-function(BaseController, JSONModel, History, MessageToast){
+function(BaseController, JSONModel, History, MessageToast, formatter){
   // "use strict";
 
   return BaseController.extend("victoria.controller.customCalculation", {
-	// onInit : function () {
+  formatter:formatter,
   onSubmit: function (evt) {
         $(function() {
                 $('input:text:first').focus();
@@ -24,7 +25,7 @@ function(BaseController, JSONModel, History, MessageToast){
                 });
             });
   },
-
+	onInit : function () {
     // var oViewModel1 = new JSONModel({
     //      "First": "",
     //         "Second": "",
@@ -42,6 +43,13 @@ function(BaseController, JSONModel, History, MessageToast){
     //         "Kaccha Silver R": ""
     //
     //         });
+    //         this.setModel(oViewModel1, "customModel");
+            // var oViewDetailModel = new JSONModel({
+            //   "buttonText" : "Save",
+            //   "deleteEnabled" : false
+            //
+            // });
+            // this.setModel(oViewDetailModel, "viewModel");
     // this.setModel(oViewModel1, "calculationModel");
 
 //     var oViewDetailModel = new JSONModel({
@@ -50,19 +58,18 @@ function(BaseController, JSONModel, History, MessageToast){
 //
 //     });
 //     this.setModel(oViewDetailModel, "viewModel");
-    // var oRouter = this.getRouter();
-    // oRouter.getRoute("CustomCalculation").attachMatched(this._onRouteMatched, this);
-// },
-//
-// getRouter: function () {
-// return sap.ui.core.UIComponent.getRouterFor(this);
-// },
-// _onRouteMatched : function(){
-// 	var that = this;
-// 	var viewModel = this.getView().getModel("viewModel");
-// 	// viewModel.setProperty("/codeEnabled", true);
-// 	// viewModel.setProperty("/buttonText", "Save");
-//
+  BaseController.prototype.onInit.apply(this);
+    var oRouter = this.getRouter();
+    oRouter.getRoute("Customizing").attachMatched(this._onRouteMatched, this);
+},
+
+getRouter: function () {
+return sap.ui.core.UIComponent.getRouterFor(this);
+},
+_onRouteMatched : function(){
+	var that = this;
+  var that = this;
+  that.getView().getModel("local").setProperty("/CustomCalculation");
 // 	this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
 // 	 "/CustomCalculation", "GET", {}, {}, this)
 // 		.then(function(oData) {
@@ -74,11 +81,11 @@ function(BaseController, JSONModel, History, MessageToast){
 // 				MessageToast.show("cannot fetch the data");
 // 		});
 // this.ClearCalculation();
-// },
-//
-//   ClearCalculation : function(){
-//
-//   },
+},
+
+  ClearCalculation : function(){
+
+  },
 // onChange: function(oEvent) {
 //   var oInput = oEvent.getSource();
 //    this.handleGoldSilverValidation(oInput);
@@ -86,17 +93,15 @@ function(BaseController, JSONModel, History, MessageToast){
   SaveCalculation : function(){
     var that = this;
     // var valid = true;
-    var oGold = this.getView().byId("idGold").getValue();
-    var oSilver = this.getView().byId("idSilver").getValue();
-    var oGold1 = parseInt(oGold, 10);
-    var oSilver1 = parseInt(oSilver, 10);
-      var oreturn = that.handleGoldSilverValidation(oGold1, oSilver1);
+    var oGoldId = this.getView().byId("idGold")
+    var oSilverId = this.getView().byId("idSilver");
+      var oreturn = that.handleGoldSilverValidation(oGoldId, oSilverId);
       // if(!handleGoldSilverValidation(oGold1, oSilver1)) {
       //   return false;
       // }
  if (oreturn == true){
     that.getView().setBusy(true);
-    var myData = this.getView().getModel("local").getProperty("/CustomCalculation");
+        var myData = this.getView().getModel("local").getProperty("/CustomCalculation");
     // myData.First = this.getView().byId("idFirst").getValue();
     // myData.Second = this.getView().byId("idSecond").getValue();
     // myData.Gold = this.getView().byId("idGold").getValue();
@@ -111,8 +116,47 @@ function(BaseController, JSONModel, History, MessageToast){
     // myData.SilverReturns1 = this.getView().byId("idSilverR1").getValue();
     // myData.KacchaGoldR = this.getView().byId("idKacchaGoldR").getValue();
     // myData.KacchaSilverR = this.getView().byId("idKacchaSilverR").getValue();
+    var i = 0;
+    var id  = that.getView().getModel("local").getProperty("/CustomCalculation")[i].id;
+    var myUrl = "/CustomCalculations('" + id + "')"
      // this.getView().getModel("local").setProperty("/CustomCalculation", myData);
      // var calculationJson = this.getView().getModel("calculationModelInfo").getData().results;
+     this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
+      myUrl, "GET", {}, {}, this)
+       .then(function(oData) {
+         var oModelCustom = new JSONModel();
+     oModelCustom.setData(oData);
+     that.getView().setModel(oModelCustom, "CustomCalculationInfo");
+     var found = 1;
+
+       }).catch(function(oError) {
+           MessageToast.show("cannot fetch the data");
+       });
+     // var customModel = this.getView().getModel("CustomCalculation");
+     // var customId = "/CustomCalculations("+that.getModel("CustomCalculation").getProperty("/Id")+ ")";
+     //  var customJson = this.getView().getModel("CustomCalculationInfo");
+     // function getCustomId(customId) {
+     //   return customJson.filter(
+     //        function (myData) {
+     //          return myData.id === customId;
+     //        }
+     //      );
+     //    }
+     //
+     //    var found = getCustomId(customId);
+        if(found > 0){
+
+          this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
+           "/CustomCalculations('"+found[0].id+"')", "PUT", {}, myData , this)
+            .then(function(oData) {
+            MessageToast.show("Data saved successfully");
+            that._onRouteMatched();
+            }).catch(function(oError) {
+                MessageToast.show("Data could not be saved");
+            });
+
+        }
+        else {
           this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
            "/CustomCalculations", "POST", {}, myData , this)
             .then(function(oData) {
@@ -123,62 +167,63 @@ function(BaseController, JSONModel, History, MessageToast){
               that.getView().setBusy(false);
               var oPopover = that.getErrorMessage(oError);
             });
+          }
             ;
 }
 // else {
 //
 // }
-}
-  //       /* =========================================================== */
-  //       /* internal methods                                            */
-  //       /* =========================================================== */
-  //
-  //       /**
-  //        * Binds the view to the object path.
-  //        * @function
-  //        * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
-  //        * @private
-  //        */
-  //       _onObjectMatched : function (oEvent) {
-  //         var sObjectId =  oEvent.getParameter("arguments").objectId;
-  //         this.getModel().metadataLoaded().then( function() {
-  //           var sObjectPath = this.getModel().createKey("city", {
-  //             Id :  sObjectId
-  //           });
-  //           this._bindView("/" + sObjectPath);
-  //         }.bind(this));
-  //
-  //
-  // },
-  // /**
-  //  * Binds the view to the object path.
-  //  * @function
-  //  * @param {string} sObjectPath path to the object to be bound
-  //  * @private
-  //  */
-  // _bindView : function (sObjectPath) {
-  //   var oViewModel1 = this.getModel("objectView"),
-  //     oDataModel = this.getModel();
-  //
-  //   this.getView().bindElement({
-  //     path: sObjectPath,
-  //     events: {
-  //       change: this._onBindingChange.bind(this),
-  //       dataRequested: function () {
-  //         oDataModel.metadataLoaded().then(function () {
-  //           // Busy indicator on view should only be set if metadata is loaded,
-  //           // otherwise there may be two busy indications next to each other on the
-  //           // screen. This happens because route matched handler already calls '_bindView'
-  //           // while metadata is loaded.
-  //           oViewModel1.setProperty("/busy", true);
-  //         });
-  //       },
-  //       dataReceived: function () {
-  //         oViewModel1.setProperty("/busy", false);
-  //       }
-      // }
+},
+        /* =========================================================== */
+        /* internal methods                                            */
+        /* =========================================================== */
+
+        /**
+         * Binds the view to the object path.
+         * @function
+         * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+         * @private
+         */
+        _onObjectMatched : function (oEvent) {
+          var sObjectId =  oEvent.getParameter("arguments").objectId;
+          this.getModel().metadataLoaded().then( function() {
+            var sObjectPath = this.getModel().createKey("city", {
+              Id :  sObjectId
+            });
+            this._bindView("/" + sObjectPath);
+          }.bind(this));
+
+
+  },
+  /**
+   * Binds the view to the object path.
+   * @function
+   * @param {string} sObjectPath path to the object to be bound
+   * @private
+   */
+  _bindView : function (sObjectPath) {
+    var oViewModel1 = this.getModel("objectView"),
+      oDataModel = this.getModel();
+
+    this.getView().bindElement({
+      path: sObjectPath,
+      events: {
+        change: this._onBindingChange.bind(this),
+        dataRequested: function () {
+          oDataModel.metadataLoaded().then(function () {
+            // Busy indicator on view should only be set if metadata is loaded,
+            // otherwise there may be two busy indications next to each other on the
+            // screen. This happens because route matched handler already calls '_bindView'
+            // while metadata is loaded.
+            oViewModel1.setProperty("/busy", true);
+          });
+        },
+        dataReceived: function () {
+          oViewModel1.setProperty("/busy", false);
+        }
+      }
     });
-  // },
-// });
+  },
+});
 }
 );

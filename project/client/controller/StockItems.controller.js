87b85a2b4d3ada9b44d99fb56c22fec3a1da3b1c
Navdeep <navdeep.kaur01@sap.com>
2020-03-24 15:47:21 -0400

    stock item screen action items

--- project/client/controller/StockItems.controller.js
+++ project/client/controller/StockItems.controller.js
@@ -1,26 +1,111 @@
 /*global location*/
 sap.ui.define(
-    ["victoria/controller/BaseController", "sap/ui/model/Filter"],
-    function (BaseController, Filter) {
+    ["victoria/controller/BaseController", "sap/ui/model/Filter","victoria/models/formatter"],
+    function (BaseController, Filter, formatter) {
         "use strict";
 
         return BaseController.extend("victoria.controller.StockItems", {
+          formatter:formatter,
+
           onAfterRendering: function(){
             this.getView().byId("idDate").setDateValue(new Date());
-          },
-
-          onConfirm:function(oEvent){
-            var that = this;
-            debugger;
-            var orderNo = oEvent.getParameter("selectedItem").getLabel();
-            var orderId = oEvent.getParameter("selectedItem").getBindingContextPath().split("'")[1];
-            this.getView().getModel("local").setProperty("/StockItemsData/Order", orderNo);
-            this.getView().getModel("local").setProperty("/StockItemsData/OrderNo",orderId);
           },
 
           valueHelpOrder:function(oEvent){
             this.orderPopup(oEvent);
           },
+
+          decimalvalidator: function (oEvent) {
+                  debugger;
+                  if(oEvent.getSource().getValue() <= 0 ){
+                    debugger;
+                    oEvent.getSource().setValueState(sap.ui.core.ValueState.Error);
+                    dialogSave.setEnabled(false);
+                  }
+                  else{
+                    oEvent.getSource().setValueState(sap.ui.core.ValueState.None);
+                    dialogSave.setEnabled(true);
+                  }
+                if(oEvent.mParameters.id==="__component0---idStockItems--idWeight"){
+
+                $(function() {
+                  $('input').on('input.idWeight',function(event) {
+                    if(event.currentTarget.id=="__component0---idStockItems--idWeight-inner"){
+                        debugger;
+                    this.value = this.value.match(/^[+-]?\d{0,6}(\.\d{0,3})?/)[0];
+                  }
+                  });
+                });
+                }
+          },
+
+          onConfirm:function(oEvent){
+            var that = this;
+            debugger;
+            var selectItem = oEvent.getParameter("selectedItem");
+            if(selectItem){
+             orderNo = selectItem.getLabel();
+            }
+            var orderId = oEvent.getParameter("selectedItem").getBindingContextPath().split('/')[2];
+            var index = null;
+            if(orderId){
+              index = parseInt(orderId);
+            }
+            this.getView().getModel("local").setProperty("/StockItemsData/Order", orderNo);
+            this.getView().getModel("local").setProperty("/StockItemsData/OrderNo",that.getView().getModel("temp").oData.items[index].id);
+          },
+
+          onMaterialSelect: function(oEvent) {
+            debugger;
+            var that = this;
+            var selectedMatData = oEvent.getParameter("selectedItem").getModel().getProperty(oEvent.getParameter("selectedItem").getBindingContext().getPath());
+
+            this.getView().getModel("local").setProperty("/StockItemsData/Material",selectedMatData.id);
+            if(selectedMatData.HindiName){
+              this.getView().byId("matName").setText(selectedMatData.HindiName);
+            }
+            else{
+              this.getView().byId("matName").setText(selectedMatData.ProductName);
+            }
+
+            var oFilter = new sap.ui.model.Filter('Material', sap.ui.model.FilterOperator.Contains, selectedMatData.id);
+            this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
+              "/StockItems", "GET", {filters: [oFilter]}, {}, this)
+              .then(function(oData) {
+              debugger;
+              var items = oData.results;
+              var aQuantity = items.map((i)=> {return i.Qty});
+              var iQuantity =  aQuantity.reduce((a, b) => a + b, 0);
+              var aWeight = items.map((i)=> {return i.Weight});
+              var iWeight =  aWeight.reduce((a, b) => a + b, 0);
+              var MaterialData = that.allMasterData;
+              console.log(MaterialData)
+              that.getView().byId("idQ").setText(parseFloat(iQuantity.toFixed(0)));
+              that.getView().byId("idW").setText(parseFloat(iWeight.toFixed(3)));
+              var updatedItems = items.map((i) => { i.MaterialName = selectedMatData.ProductCode; return i;})
+              var json = new sap.ui.model.json.JSONModel();
+              json.setData({
+                "StockItems": items
+              });
+              var table = that.getView().byId("idTable1");
+              table.setModel(json);
+
+              }).catch(function(oError) {
+              });
+          },
+
+          validateValue: function(oEvent){
+            if(oEvent.getSource().getValue() <= 0 ){
+              debugger;
+              oEvent.getSource().setValueState(sap.ui.core.ValueState.Error);
+              dialogSave.setEnabled(false);
+            }
+            else{
+              oEvent.getSource().setValueState(sap.ui.core.ValueState.None);
+              dialogSave.setEnabled(true);
+            }
+          },
+
 
           refreshModel: function(oEvent){
             var table = this.getView().byId("idTable1");
@@ -33,14 +118,13 @@ sap.ui.define(
             }
 
           },
-
           orderPopup: function(oEvent) {
+            var that = this;
             this.orderNoPopup = new sap.ui.xmlfragment("victoria.fragments.popup", this);
             this.getView().addDependent(this.orderNoPopup);
             var title = this.getView().getModel("i18n").getProperty("orderSearch");
             this.orderNoPopup.setTitle(title);
             var orderDate = this.byId("idDate").getValue();
-            var customer = this.getView().getModel('local').getProperty('/orderHeader').Customer;
             var dateFrom = new Date(orderDate);
             dateFrom.setHours(0, 0, 0, 1)
             var dateTo = new Date(orderDate);
@@ -52,17 +136,41 @@ sap.ui.define(
               filters: [oFilter1, oFilter2],
               and: true
             });
-            this.orderNoPopup.bindAggregation("items", {
-                path: '/OrderHeaders',
-                filters: orFilter,
-                template: new sap.m.DisplayListItem({
-                label: "{OrderNo}",
-                value: {
-                  path: 'Customer',
-                  formatter: this.getCustomerName.bind(this)
-                  }
-                })
+
+            this.ODataHelper.callOData(this.getOwnerComponent().getModel(),
+                   "/OrderHeaders",
+                   "GET", {filters: [orFilter]}, {}, this)
+            .then(function(oData) {
+              var results  =  oData.results.slice();
+              that.ODataHelper.callOData(that.getOwnerComponent().getModel(), "/WSOrderHeaders",
+                                        "GET", {filters: [orFilter]}, {}, that)
+              .then(function(oData) {
+                var results1  = oData.results.slice();
+                var abc = results.concat(results1);
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData({
+									"items": abc
+								});
+                that.getView().setModel(oModel, "temp");
+                }).catch(function(oError) {
+                console.log(oError);
               });
+            })
+            .catch(function(oError) {
+              console.log(oError);
+            })
+
+              this.orderNoPopup.bindAggregation("items", {
+                  path: 'temp>/items',
+                  filters: orFilter,
+                  template: new sap.m.DisplayListItem({
+                  label: "{temp>OrderNo}",
+                  value: {
+                    path: 'temp>Customer',
+                    formatter: this.getCustomerName.bind(this)
+                    }
+                  })
+                });
             this.orderNoPopup.open();
           },
 
@@ -70,27 +178,41 @@ sap.ui.define(
  							debugger;
  							var that = this;
               var data = this.getView().getModel("local").getProperty("/StockItemsData");
-              console.log(data);
-							this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/StockItems",
-							                          "POST", {}, data, this)
-							.then(function(oData) {
-								that.getView().setBusy(false);
-								debugger;
-								sap.m.MessageToast.show("Data Saved Successfully");
-                that.getView().byId("idOrderNo").setValue("");
-                that.getView().byId("idMatCode").setValue("");
-                that.getView().byId("idQuantity").setValue("");
-                that.getView().byId("idWeight").setValue("");
-                that.getView().byId("idRemarks").setValue("");
-                that.getView().byId("idQ").setText("");
-                that.getView().byId("idW").setText("");
-							}).catch(function(oError) {
-								that.getView().setBusy(false);
-							});
+              console.log(data)
+
+              if(this.getView().byId("idMatCode").getValue() === "" ){
+                sap.m.MessageBox.show("Please enter the Material");
+              }
+              else if(this.getView().byId("idQuantity").getValue() <= 0){
+                sap.m.MessageBox.show("Please enter the valid Quantity");
+              }
+              else if(
+              this.getView().byId("idWeight").getValue() <= 0 ){
+                sap.m.MessageBox.show("Please enter the valid Weight");
+              }
+              else{
+                this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/StockItems",
+  							                          "POST", {}, data, this)
+  							.then(function(oData) {
+  								that.getView().setBusy(false);
+  								debugger;
+  								sap.m.MessageToast.show("Data Saved Successfully");
+                  that.getView().byId("idOrderNo").setValue("");
+                  that.getView().byId("idMatCode").setValue("");
+                  that.getView().byId("idQuantity").setValue("");
+                  that.getView().byId("idWeight").setValue("");
+                  that.getView().byId("idRemarks").setValue("");
+                  that.getView().byId("idQ").setText("");
+                  that.getView().byId("idW").setText("");
+                  that.getView().byId("matName").setText("");
+  							}).catch(function(oError) {
+  								that.getView().setBusy(false);
+  							});
+              }
               var table = that.getView().byId("idTable1");
               var currentModel = that.getView().getModel();
               table.setModel(currentModel);
-              this.getView().byId("idDelete").setEnabled(true);
+              that.getView().byId("idDelete").setEnabled(true);
 						},
 
           onClear: function(){
@@ -139,36 +261,6 @@ sap.ui.define(
                 );
             },
 
-          productCodeCheck: function(oEvent){
-              debugger;
-              var that = this;
-              var matId = oEvent.getParameter("selectedItem").getBindingContext().getPath().split("'")[1];
-              var data = this.getView().getModel("local").getProperty("/StockItemsData");
-              data.Material = matId;
-              var oFilter = new sap.ui.model.Filter("Material","EQ", matId);
-              this.getView().byId("idTable1").getBinding("items").filter(oFilter);
-              this.getView().byId("idDelete").setEnabled(false);
-              this.ODataHelper.callOData(this.getOwnerComponent().getModel(), "/StockItems",
-							                          "GET", {oFilter}, {}, this)
-							.then(function(oData) {
-								that.getView().setBusy(false);
-								console.log(oData.results)
-                var items = oData.results.filter((i)=>{return i.Material === matId});
-                var aQuantity = items.map((i)=> {return i.Qty});
-                var iQuantity =  aQuantity.reduce((a, b) => a + b, 0);
-                var aWeight = items.map((i)=> {return i.Weight});
-                var iWeight =  aWeight.reduce((a, b) => a + b, 0);
-                that.getView().byId("idQ").setText(parseFloat(iQuantity.toFixed(0)));
-                that.getView().byId("idW").setText(parseFloat(iWeight.toFixed(0)));
-                var json = new sap.ui.model.json.JSONModel();
-                json.setData({
-									"StockItems": items
-								});
-                var table = that.getView().byId("idTable1");
-                table.setModel(json);
-							});
-            },
-
           onUpdateFinished: function (oEvent) {
          		  debugger;
               var oTable = oEvent.getSource();
@@ -178,10 +270,11 @@ sap.ui.define(
          			this.getView().byId("idTitle").setText(title + " " + "(" + noOfItems + ")");
                for (var i=0; i < noOfItems; i++) {
                  debugger;
-                 var materialId = oTable.getItems()[i].getCells()[1].getText();
+                 var materialId = oTable.getItems()[i].getCells()[2].getText();
+                 var orderId = oTable.getItems()[i].getCells()[1].getText();
                  var material = this.allMasterData.materials[materialId];
                  if(material){
-                   oTable.getItems()[i].getCells()[1].setText(material.ProductCode);
+                   oTable.getItems()[i].getCells()[2].setText(material.ProductCode);
                  }
                }
             }

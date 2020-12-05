916cb12182c4ee71b476a2abc26d1e6e223cf171
anubajaj2 <anurag.bajaj02@gmail.com>
2020-12-05 16:53:58 +0530

    rollback changes

--- project/server/server.js
+++ project/server/server.js
@@ -2585,7 +2585,13 @@ app.start = function() {
 							var dateObj = new Date();
 							console.log(dDateStart);
 							console.log(dDateEnd);
-							app.models.StockItem.find({where: {and: [{Date: {gt:req.query.date}}, {Date: {lt:dDateEnd}}]}}).then(function(stockItems, err) {
+							app.models.StockItem.find({
+								where: {
+									Date: {
+										between : [dDateStart, dDateEnd]
+									}
+								}
+							}).then(function(stockItems, err) {
 								// calculating  quantity
 								console.log("Stock items " + JSON.stringify(stockItems) );
 								for (item of stockItems) {

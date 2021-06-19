e7e192db3797441fb3ac1a20b0aeb55341697d54
kuldeep singh <kuldeepsingh@soyuztechnologies.com>
2021-06-19 12:43:59 +0530

    Daybook screen is updated

--- project/src/dist/Component-dbg.js
+++ project/src/dist/Component-dbg.js
@@ -16,7 +16,6 @@ sap.ui.define(
 				// browser.driver.navigate().refresh();
         // browser.loadUI5Dependencies()
 				HashChanger.getInstance().replaceHash("");
-				debugger;
 				sap.ui.core.UIComponent.prototype.init.apply(this);
 
      			var oRouter = this.getRouter();

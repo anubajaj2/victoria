952a5ab0b0c8c4fb95d6464efd695196ce788047
soyuztechnologies <contact@soyuztechnologies.com>
2024-01-22 08:42:21 +0530

    build

--- project/src/dist/Component.js
+++ project/src/dist/Component.js
@@ -1,1 +1,2 @@
 sap.ui.define(["sap/ui/core/UIComponent","victoria/models/models","sap/ui/model/json/JSONModel","sap/ui/core/routing/HashChanger"],function(e,t,i,n){return e.extend("victoria.Component",{metadata:{manifest:"json"},init:function(){n.getInstance().replaceHash("");sap.ui.core.UIComponent.prototype.init.apply(this);var e=this.getRouter();e.initialize()},destroy:function(){}})});
+//# sourceMappingURL=Component.js.map
